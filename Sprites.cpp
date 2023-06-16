863e94b4d43fc9df941ad7c47b7c53d7388f10e8
Asus <Asus@LAPTOP-E0AS47UM>
2023-06-16 23:41:55 +0700

    add Utils header

---
+++ Sprites.cpp
@@ -0,0 +1,13 @@
+#include "Sprites.h"
+#include "Game.h"
+#include "Utils.h"
+
+CSprite::CSprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex)
+{
+	this->id = id;
+	this->left = left;
+	this->top = top;
+	this->right = right;
+	this->bottom = bottom;
+	this->texture = tex;
+}
